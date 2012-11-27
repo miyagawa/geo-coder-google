813521e4ecf0f4c6b50c0c7de4f13655d1d565da
Justin Hunter <justin.d.hunter@gmail.com>
2012-11-26 22:47:30 -0500

    bump version

--- lib/Geo/Coder/Google.pm
+++ lib/Geo/Coder/Google.pm
@@ -2,7 +2,7 @@ package Geo::Coder::Google;
 
 use strict;
 use warnings;
-our $VERSION = '0.11';
+our $VERSION = '0.11_01';
 
 sub new {
     my ($self, %param) = @_;
--- lib/Geo/Coder/Google/V2.pm
+++ lib/Geo/Coder/Google/V2.pm
@@ -2,7 +2,7 @@ package Geo::Coder::Google::V2;
 
 use strict;
 use warnings;
-our $VERSION = '0.11';
+our $VERSION = '0.11_01';
 
 use Carp;
 use Encode;
